2f46336060490403a3963747d618a7db53c556f8
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 20:59:32 +0430

    paste form from bootstrap

--- src/Pages/LoginRegister/Login/index.jsx
+++ src/Pages/LoginRegister/Login/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export default function Login() {
+export default function Login({ handlePageType }) {
   return (
-    <div>Login</div>
-  )
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+      
+    </form>
+  );
 }
