6f1b5c4cb9ef695ef4d50fb98dc695a3015fbafa
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 16:16:27 +0430

    Update index.jsx

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function ProductDetails() {
+    const {id}=useParams()
   return (
     <div>ProductDetails</div>
   )
