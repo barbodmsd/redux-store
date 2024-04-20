2cda83b85b20e2b084e809b8e92dc76c2a9d6147
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 14:48:31 +0430

    end store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./Slices/Auth";
+import cartSlice from "./Slices/Cart";
 
-const store=configureStore({
-    reducer:{
-        authSlice
-    }
-})
-export default store
+const store = configureStore({
+  reducer: {
+    authSlice,
+    cartSlice,
+  },
+});
+export default store;
