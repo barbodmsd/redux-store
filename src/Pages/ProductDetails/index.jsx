fdff969f15d82005287f0580e6da21d815ff9147
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 16:37:00 +0430

    buuton

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import style from './ProductDetails.module.css'
+import style from "./ProductDetails.module.css";
+import Loading from "../../Components/Loading";
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState();
@@ -13,34 +14,48 @@ export default function ProductDetails() {
           headers: { "content-type": "application/json" },
         });
         const data = await res.json();
+        
         data ? setProduct(data) : navigate("/not-found-product-details");
       } catch (error) {
         alert("Error Product details fetch ");
       }
     })();
   }, [id]);
+
   return (
-    <div className={`card mb-3 ${style.cardDetailsContainer} `}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src={product.image} className="img-fluid rounded-start" alt={product.title} />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{product.title}</h5>
-            <p className="card-text">
-              {product.description}
-              <br/>
-              Price : {product.price}
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                Last updated 3 mins ago
-              </small>
-            </p>
+    <>
+      {product ? (
+        <div className={`card mb-3 my-2 mx-auto  ${style.cardDetailsContainer} `}>
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={product.image}
+                className="img-fluid rounded-start"
+                alt={product.title}
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">
+                  {product.description}
+                  <br />
+                <strong>  Price : {product.price}</strong>
+                <button className="btn btn-danger" onClick={()=>}></button>
+                <button className="btn btn-danger" onClick={()=>}></button>
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
