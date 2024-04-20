d92592d0f4fac48ee11c1f2e3a84504bf753e742
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 15:03:04 +0430

    end footer cmp

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import style from "./Footer.module.css";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div
+      className={`d-flex justify-content-center align-items-center ${style.footerContainer}`}
+    >
+      <h2>Footer</h2>
+    </div>
+  );
 }
