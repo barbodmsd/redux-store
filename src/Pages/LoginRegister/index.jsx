c3b9f724f8f8f97c7eebb5932453a4bb2d2c19a3
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 17:25:07 +0430

    start useeffect

--- src/Pages/LoginRegister/index.jsx
+++ src/Pages/LoginRegister/index.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function LoginRegister() {
+  useEffect(()=>{
+    
+  },[])
   return (
     <div>LoginRegister</div>
   )
