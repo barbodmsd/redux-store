10564466ff12080782ef338de804c744eb974edd
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 16:05:02 +0430

    return products cmp

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from "react";
+import Card from "../../Components/Card";
+import Loading from "../../Components/Loading";
 
 export default function Products() {
   const [products, setProducts] = useState();
   useEffect(() => {
     (async () => {
       try {
-        const res = await fetch(
-          "https://sephora.p.rapidapi.com/products/v2/list?number=1&size=30&country=SG&language=en-SG&sort=sales",
-          {
-            method: "GET",
-            headers: {
-              "X-RapidAPI-Key":
-                "384ba6ddb4mshd30ea53794d2c1bp16a8e9jsn1941df36d024",
-              "X-RapidAPI-Host": "sephora.p.rapidapi.com",
-            },
-          }
-        );
+        const res = await fetch("https://fakestoreapi.com/products", {
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+          },
+        });
         const data = await res.json();
         setProducts(data);
       } catch (error) {
@@ -23,6 +20,18 @@ export default function Products() {
       }
     })();
   }, []);
-  
-  return <div>Products</div>;
+  const items = products?.map((e, index) => (
+    <Card key={index} img={e.image} id={e.id} name={e.title} price={e.price} />
+  ));
+  return (
+    <>
+      {products ? (
+        <div className="d-flex justify-content-center align-items-center gap-3 flex-wrap ">
+          {items}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
