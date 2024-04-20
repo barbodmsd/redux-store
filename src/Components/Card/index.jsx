32f1d5c8bbf6583f3e45f561f3f7ae4bfa3c3dab
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 16:08:47 +0430

    end products cmp

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import style from './Card.module.css'
 export default function Card({ img, name, price, id }) {
   return (
-    <div className="card">
+    <div className={`card ${style.cardContainer}`}>
       <img src={img} className="card-img-top" alt={name} />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
