db03a11e94a1df646542393fe4fe3298c5c276a9
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 16:22:39 +0430

    end useefefct

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function ProductDetails() {
     const {id}=useParams()
     const [product,setProduct]=useState()
+    const navigate=useNavigate()
+    useEffect(()=>{
+        (async()=>{
+            try {
+                const res=await fetch(`https://fakestoreapi.com/products/${id}`,{
+                    method:'GET',
+                    headers:{'content-type':'application/json'}
+                })
+                const data=await res.json()
+                data?setProduct(data):navigate('/not-found-product-details')
+            } catch (error) {
+                alert('Error Product details fetch ')
+            }
+        })()
+    },[id])
   return (
     <div>ProductDetails</div>
   )
