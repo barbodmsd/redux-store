2c34cd851678951aa18556121ae9d4390cb5bc6c
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 15:03:53 +0430

    end footer cmp

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,36 +1,56 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logout } from '../../Store/Slices/Auth'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../../Store/Slices/Auth";
 
 export default function Navbar() {
-    const {token}=useSelector(state=>state.authSlice)
-    const dispatch=useDispatch()
+  const { token } = useSelector((state) => state.authSlice);
+  const dispatch = useDispatch();
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">Navbar</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/products">Products</Link>
-          </li>
-         {token? <li className="nav-item">
-            <span className="nav-link"onClick={()=>dispatch(logout())} >Logout</span>
-          </li>: <li className="nav-item">
-            <Link className="nav-link" to="/login-register">Login/Register</Link>
-          </li>}
-          
-        </ul>
-        
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">
+                Products
+              </Link>
+            </li>
+            {token ? (
+              <li className="nav-item">
+                <span className="nav-link" onClick={() => dispatch(logout())}>
+                  Logout
+                </span>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link className="nav-link" to="/login-register">
+                  Login/Register
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-  )
+    </nav>
+  );
 }
