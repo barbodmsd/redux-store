23ee12149233e544522f96f80c909fcc2796940c
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 17:32:31 +0430

    paste login form

--- src/Pages/LoginRegister/index.jsx
+++ src/Pages/LoginRegister/index.jsx
@@ -1,10 +1,36 @@
-import React, { useEffect } from 'react'
+import React from "react";
 
 export default function LoginRegister() {
-  useEffect(()=>{
-    
-  },[])
   return (
-    <div>LoginRegister</div>
-  )
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
