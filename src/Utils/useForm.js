8f5447343eb370b7e70b9d098cd7cb8068ff5074
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 18:08:49 +0430

    end use From hook

---
+++ src/Utils/useForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useForm = () => {
+  const [fields, setFields] = useState();
+  const handleChange = (e) => {
+    const { target } = e;
+    setFields({
+      ...fields,
+      [target.name]: target.value,
+    });
+  };
+
+  return [fields, handleChange];
+};
+
+
+export default useForm
