dbf44c6136676e3c9357cc1ec67a892d778a6893
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 21:26:48 +0430

    paste table

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Cart() {
   return (
-    <div>Cart</div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+  </tbody>
+</table>
   )
 }
