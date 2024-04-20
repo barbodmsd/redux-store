ce57866bbb13749ce714d3e80fb367bce95682fa
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-20 20:56:38 +0430

    register cmp

--- src/Pages/LoginRegister/Register/index.jsx
+++ src/Pages/LoginRegister/Register/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function Register() {
+export default function Register({ handlePageType }) {
   return (
-    <div>Register</div>
-  )
+    <div>
+      <button className="btn btn-success " onClick={handlePageType}>
+        Already have An Account?
+      </button>
+    </div>
+  );
 }
